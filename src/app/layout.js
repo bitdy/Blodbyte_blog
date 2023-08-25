cc0f8a1623be5358d5a1336d88a9353576197a77
Homayoun <homayoun763@gmail.com>
2023-08-25 07:42:20 +0330

    Fix Own layout issiue

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,6 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      layout
       <body className={inter.className}>{children}</body>
     </html>
   )
