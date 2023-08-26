a464f2abe67ed5ad09053724265949ca59487731
Homayoun <homayoun763@gmail.com>
2023-08-26 21:33:00 +0330

    Create blogs

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,10 +1,104 @@
 import React from 'react'
 import styles from "./page.module.css"
+import Button from '@/components/Button/Button'
+import Image from 'next/image'
+import Link from 'next/link'
 
 
 const Blog = () => {
   return (
-    <div className={styles.container}>Blog</div>
+    <div className={styles.mainContainer}>
+      <Link href="/blog/testId" className={styles.container} >
+        <div className={styles.imageContainer}>
+          <Image
+            src="/about.jpeg"
+            alt=""
+            width={400}
+            height={250}
+            className={styles.image}
+          />
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Lorem ipsum dolor, sit amet consectetur adipisicing.</p>
+        </div>
+      </Link>
+      <Link href="/blog/testId" className={styles.container} >
+        <div className={styles.imageContainer}>
+          <Image
+            src="/about.jpeg"
+            alt=""
+            width={400}
+            height={250}
+            className={styles.image}
+          />
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Lorem ipsum dolor, sit amet consectetur adipisicing.</p>
+        </div>
+      </Link>
+      <Link href="/blog/testId" className={styles.container} >
+        <div className={styles.imageContainer}>
+          <Image
+            src="/about.jpeg"
+            alt=""
+            width={400}
+            height={250}
+            className={styles.image}
+          />
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Lorem ipsum dolor, sit amet consectetur adipisicing.</p>
+        </div>
+      </Link>
+      <Link href="/blog/testId" className={styles.container} >
+        <div className={styles.imageContainer}>
+          <Image
+            src="/about.jpeg"
+            alt=""
+            width={400}
+            height={250}
+            className={styles.image}
+          />
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Lorem ipsum dolor, sit amet consectetur adipisicing.</p>
+        </div>
+      </Link>
+      <Link href="/blog/testId" className={styles.container} >
+        <div className={styles.imageContainer}>
+          <Image
+            src="/about.jpeg"
+            alt=""
+            width={400}
+            height={250}
+            className={styles.image}
+          />
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Lorem ipsum dolor, sit amet consectetur adipisicing.</p>
+        </div>
+      </Link>
+      <Link href="/blog/testId" className={styles.container} >
+        <div className={styles.imageContainer}>
+          <Image
+            src="/about.jpeg"
+            alt=""
+            width={400}
+            height={250}
+            className={styles.image}
+          />
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Lorem ipsum dolor, sit amet consectetur adipisicing.</p>
+        </div>
+      </Link>
+  </div>
   )
 }
 
