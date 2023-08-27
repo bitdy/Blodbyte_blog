c1cf4a405be8a03ae884a6ad327c177ccee80ba7
Homayoun <homayoun763@gmail.com>
2023-08-28 00:48:14 +0330

    Fetch the data from the jsonplaceholder

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -3,11 +3,24 @@ import styles from "./page.module.css"
 import Image from 'next/image'
 import Link from 'next/link'
 
+async function getData() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  console.log(res)
 
-const Blog = () => {
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
+const Blog = async () => {
+
+  const data = await getData();
   return (
     <div className={styles.mainContainer}>
-      <Link href='/blog/testId' className={styles.container} >
+      {data.map(item=>(
+        <Link href='/blog/testId' className={styles.container} key={item.id}>
         <div className={styles.imageContainer}>
           <Image
             src="/about.jpeg"
@@ -18,85 +31,12 @@ const Blog = () => {
           />
         </div>
         <div className={styles.content}>
-          <h1 className={styles.title}>Tittle</h1>
+          <h1 className={styles.title}>{item.title}</h1>
           <p className={styles.desc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, in.</p>
         </div>
       </Link>
-      <Link href='/blog/testId' className={styles.container} >
-        <div className={styles.imageContainer}>
-          <Image
-            src="/about.jpeg"
-            alt=""
-            width={400}
-            height={250}
-            className={styles.image}
-          />
-        </div>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Tittle</h1>
-          <p className={styles.desc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, in.</p>
-        </div>
-      </Link>
-      <Link href='/blog/testId' className={styles.container} >
-        <div className={styles.imageContainer}>
-          <Image
-            src="/about.jpeg"
-            alt=""
-            width={400}
-            height={250}
-            className={styles.image}
-          />
-        </div>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Tittle</h1>
-          <p className={styles.desc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, in.</p>
-        </div>
-      </Link>
-      <Link href='/blog/testId' className={styles.container} >
-        <div className={styles.imageContainer}>
-          <Image
-            src="/about.jpeg"
-            alt=""
-            width={400}
-            height={250}
-            className={styles.image}
-          />
-        </div>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Tittle</h1>
-          <p className={styles.desc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, in.</p>
-        </div>
-      </Link>
-      <Link href='/blog/testId' className={styles.container} >
-        <div className={styles.imageContainer}>
-          <Image
-            src="/about.jpeg"
-            alt=""
-            width={400}
-            height={250}
-            className={styles.image}
-          />
-        </div>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Tittle</h1>
-          <p className={styles.desc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, in.</p>
-        </div>
-      </Link>
-      <Link href='/blog/testId' className={styles.container} >
-        <div className={styles.imageContainer}>
-          <Image
-            src="/about.jpeg"
-            alt=""
-            width={400}
-            height={250}
-            className={styles.image}
-          />
-        </div>
-        <div className={styles.content}>
-          <h1 className={styles.title}>Tittle</h1>
-          <p className={styles.desc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, in.</p>
-        </div>
-      </Link>
+        ))}
+      
   </div>
   )
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,6 +3,7 @@
 import Link from "next/link";
 import React from "react";
 import styles from "./page.module.css"
+import DarkModeToggle from "../DarkModeToggle/DarkModeToggle";
 
 
 const links = [
@@ -43,6 +44,7 @@ const Navbar = () => {
     <div className={styles.container}>
       <Link href="/" className={styles.logo}>Blod Byte</Link>
       <div className={styles.links}>
+        <DarkModeToggle />
         {links.map((link) => (
           <Link key={link.id} href={link.url} className={styles.link}>
             {link.title}
